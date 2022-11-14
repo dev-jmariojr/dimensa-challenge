7a0b683856fa1b3d777564399f5588f64cee6c33
JJ <jmario_junior@hotmail.com>
2022-11-13 23:11:58 -0300

    tests

--- src/main/java/br/com/jj/challenge/model/Contacts.java
+++ src/main/java/br/com/jj/challenge/model/Contacts.java
@@ -1,12 +1,18 @@
 package br.com.jj.challenge.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Contacts {
 
--- src/test/java/br/com/jj/challenge/service/ContactsServiceTest.java
+++ src/test/java/br/com/jj/challenge/service/ContactsServiceTest.java
@@ -1,32 +1,4 @@
 package br.com.jj.challenge.service;
+public class ContactsServiceTest {
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ContactsServiceTest {
-
-    @Test
-    void listAll() {
-    }
-
-    @Test
-    void listAllWithParam() {
-    }
-
-    @Test
-    void find() {
-    }
-
-    @Test
-    void add() {
-    }
-
-    @Test
-    void update() {
-    }
-
-    @Test
-    void remove() {
-    }
 }

b30708b96c9c4d45eaddce022dc0414bd9e4cb91
JJ <jmario_junior@hotmail.com>
2022-11-14 17:47:22 -0300

    clear

--- src/main/java/br/com/jj/challenge/controller/ContactsController.java
+++ src/main/java/br/com/jj/challenge/controller/ContactsController.java
@@ -3,8 +3,6 @@ package br.com.jj.challenge.controller;
 import br.com.jj.challenge.model.Contacts;
 import br.com.jj.challenge.service.ContactsService;
 import br.com.jj.challenge.service.IContactsService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +14,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@Api
 @RequestMapping("v1/contacts")
 public class ContactsController {
 
@@ -30,7 +27,6 @@ public class ContactsController {
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value = "API Informations")
     @GetMapping("/info")
     public String infoApp(){
         StringBuilder builder = new StringBuilder();
@@ -41,7 +37,6 @@ public class ContactsController {
                 .toString();
     }
 
-    @ApiOperation(value = "List of the Contacts")
     @GetMapping
     public ResponseEntity<List<Contacts>> findAll(@RequestParam(required = false) Optional<String> name){
         List<Contacts> list = contactsService.listAll(name.orElse(""));
@@ -51,7 +46,6 @@ public class ContactsController {
             return ResponseEntity.ok(list);
     }
 
-    @ApiOperation(value = "Find a contacts by id")
     @GetMapping("/{id}")
     public ResponseEntity<Contacts> findOne(@PathVariable Long id){
         Contacts contacts = contactsService.findOne(id);
@@ -61,13 +55,11 @@ public class ContactsController {
             return ResponseEntity.ok(contacts);
     }
 
-    @ApiOperation(value = "Add new contact")
     @PostMapping
     public ResponseEntity<Contacts> add(@RequestBody Contacts value){
         return ResponseEntity.ok(contactsService.add(value));
     }
 
-    @ApiOperation(value = "Edit contact data")
     @PutMapping("/{id}")
     public ResponseEntity<Contacts> update(@PathVariable Long id, @RequestBody Contacts value){
         Contacts contacts = contactsService.findOne(id);
@@ -82,7 +74,6 @@ public class ContactsController {
         }
     }
 
-    @ApiOperation(value = "Remove contact by id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Boolean> remove(@PathVariable Long id){
         Contacts contacts = contactsService.findOne(id);
